828bbdd828737df72747a1bcaa18819d98a7988a
sergiovonsik <nicolasvonsik@gmail.com>
2025-09-12 14:35:56 -0300

    sergio.

--- tp1/src/Expr.hs
+++ tp1/src/Expr.hs
@@ -1,5 +1,6 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use (,)" #-}
+{-# HLINT ignore "Use const" #-}
 module Expr
   ( Expr (..),
     recrExpr,
@@ -13,6 +14,7 @@ where
 
 import Generador
 import Histograma
+import Histograma (casilleros, histograma)
 
 -- | Expresiones aritméticas con rangos
 data Expr
@@ -64,7 +66,11 @@ eval = foldExpr (\x g -> (x,g)) (\x y g -> dameUno (x,y) g)
 -- | @armarHistograma m n f g@ arma un histograma con @m@ casilleros
 -- a partir del resultado de tomar @n@ muestras de @f@ usando el generador @g@.
 armarHistograma :: Int -> Int -> G Float -> G Histograma
-armarHistograma m n f g = error "COMPLETAR EJERCICIO 9"
+armarHistograma m n f g =  (histograma m (piso, techo) xs, gen')
+  where
+    (xs, gen') = muestra f n g
+    (piso, techo) = rango95 xs 
+
 
 -- | @evalHistograma m n e g@ evalúa la expresión @e@ usando el generador @g@ @n@ veces
 -- devuelve un histograma con @m@ casilleros y rango calculado con @rango95@ para abarcar el 95% de confianza de los valores.
@@ -100,3 +106,6 @@ constructor (Div _ _) = CEDiv
 maybeParen :: Bool -> String -> String
 maybeParen True s = "(" ++ s ++ ")"
 maybeParen False s = s
+
+
+ 
